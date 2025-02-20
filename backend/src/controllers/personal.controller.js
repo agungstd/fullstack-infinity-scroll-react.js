b8fc68fbf88cb50d8b7980d39db4923a965b6569
agungstd <apakatadunia11@gmail.com>
2025-02-20 17:09:35 +0700

    Refactor getPersonals function for cleaner code

--- backend/src/controllers/personal.controller.js
+++ backend/src/controllers/personal.controller.js
@@ -4,86 +4,28 @@ export const getPersonals = async (req, res) => {
   const last_id = parseInt(req.query.lastId) || 0;
   const limit = parseInt(req.query.limit) || 10;
   const search = req.query.search_query || "";
-  let result = [];
-  if (last_id < 1) {
-    result = await prisma.personaldata.findMany({
-      where: {
-        OR: [
-          {
-            first_name: {
-              contains: search,
-            },
-          },
-          {
-            last_name: {
-              contains: search,
-            },
-          },
-          {
-            email: {
-              contains: search,
-            },
-          },
-          {
-            gender: {
-              contains: search,
-            },
-          },
-          {
-            ip_address: {
-              contains: search,
-            },
-          },
-        ],
-      },
-      take: limit,
-      orderBy: {
-        id: "desc",
-      },
-    });
-  } else {
-    result = await prisma.personaldata.findMany({
-      where: {
-        id: {
-          lt: last_id,
-        },
-        OR: [
-          {
-            first_name: {
-              contains: search,
-            },
-          },
-          {
-            last_name: {
-              contains: search,
-            },
-          },
-          {
-            email: {
-              contains: search,
-            },
-          },
-          {
-            gender: {
-              contains: search,
-            },
-          },
-          {
-            ip_address: {
-              contains: search,
-            },
-          },
-        ],
-      },
-      take: limit,
-      orderBy: {
-        id: "desc",
-      },
-    });
-  }
+  
+  const searchConditions = {
+    OR: [
+      { first_name: { contains: search } },
+      { last_name: { contains: search } },
+      { email: { contains: search } },
+      { gender: { contains: search } },
+      { ip_address: { contains: search } }
+    ],
+  };
+
+  const queryOptions = {
+    where: last_id ? { id: { lt: last_id }, ...searchConditions } : searchConditions,
+    take: limit,
+    orderBy: { id: "desc" },
+  };
+
+  const result = await prisma.personaldata.findMany(queryOptions);
+
   res.json({
     result,
     lastId: result.length > 0 ? result[result.length - 1].id : 0,
-    hasMore: result.length >= limit ? true : false,
+    hasMore: result.length >= limit,
   });
 };
